e19a4b2d2722737f2dc0731b4adbee5aad931e91
cursedspaghetti73 <cursed.spaghetti.73@gmail.com>
2026-01-09 10:28:58 +0100

    Load shadow image from GitHub
    Added shadow image for rendering in the game.

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ import { CONFIG, gameState } from './config.js';
 import * as Renderer from './renderer.js';
 import * as Engine from './engine.js';
 
+const shadowImg = new Image();
+shadowImg.src = 'https://raw.githubusercontent.com/cursedspaghetti73/Forgotten-Wiz/main/Shadow.gif';
+// Usiamo l'URL "raw" per caricare l'immagine direttamente da GitHub
+
 // --- DOM ELEMENTS ---
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
