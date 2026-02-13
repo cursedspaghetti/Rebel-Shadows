82d8200a4c905528c7bdf9f690cff702c5cdb001
cursedspaghetti73 <cursed.spaghetti.73@gmail.com>
2026-02-13 17:19:09 +0100

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ export const CONFIG = {
         
         // Attacco RADIALE (Sventagliata)
         RADIAL: {
-            INTERVAL: 8000,
+            INTERVAL: 4000,
             COOLDOWN_P2: 0.6,
             WAVES: 4,
             BULLETS_PER_WAVE: 14,
