8d18fae2d8c7241fe017df43f2ab4c21d2e8ceea
cursedspaghetti73 <cursed.spaghetti.73@gmail.com>
2026-02-17 17:34:41 +0100

    Update main.js

--- main.js
+++ main.js
@@ -286,7 +286,7 @@ function gameLoop() {
 
     // Draw Player logic
     if (!(gameState.isInvulnerable && Math.floor(now / 100) % 2 === 0)) {
-        Renderer.drawPlayer(ctx, bookImg);
+        Renderer.drawPlayer(bookImg);
     }
 
     Enemies.drawEnemies(ctx);
