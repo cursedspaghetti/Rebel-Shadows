cab33a953aa445021a632672b24c68bf1df736cd
cursedspaghetti73 <cursed.spaghetti.73@gmail.com>
2026-02-23 18:52:52 +0100

    Update main.js

--- main.js
+++ main.js
@@ -350,7 +350,7 @@ function gameLoop() {
 
     // --- 8. UI ---
     Renderer.drawUI(ctx);
-    Renderer.drawHealthBar(ctx, gameState.HP, 100 + (gameState.addedStats["HP"] * 10), CONFIG.CANVAS_WIDTH);
+    Renderer.drawHealthBar(ctx, gameState.HP, 100, CONFIG.CANVAS_WIDTH);
 
     requestAnimationFrame(gameLoop);
 }
